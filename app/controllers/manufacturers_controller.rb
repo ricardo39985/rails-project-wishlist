6c70374e01b2653dab63343d25d73302702a2480
ricardo39985 <ricardo39985@gmail.com>
2020-03-21 15:01:33 -0400

    Add manufacturer controller

---
+++ app/controllers/manufacturers_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ManufacturersController < ApplicationController
+  def show
+    redirect_if_try_to_spoof
+    check_manufacturer_belongs_to_user
+  end
+
+  private
+
+  def check_manufacturer_belongs_to_user
+    @car = current_user.cars.find_by(id: params[:car_id])
+    @man = @car.manufacturer
+    unless params[:id] == @car.manufacturer.id.to_s
+      flash[:notice] = "Manufacturer does not belong to current car"
+      redirect_to user_car_path(current_user, @car)
+    end
+  end
+end
