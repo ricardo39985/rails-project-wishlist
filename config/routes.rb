26365f17e57d65131aa7bf629990f7b0262456ad
ricardo39985 <ricardo39985@gmail.com>
2020-03-21 10:46:53 -0400

    Added routes

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  devise_for :users, skip: [:inbound_email], controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root to: 'home#index'
+  resources :users, only: %i[show index] do
+    resources :cars do
+      resources :dealerships, only: %i[index show]
+      resources :manufacturers, only: [:show]
+    end
+  end
+
+  root to: 'home#index'
+  
+  
+end
