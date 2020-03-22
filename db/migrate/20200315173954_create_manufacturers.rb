2329acec672b046604d846806b0536699c8460d7
ricardo39985 <ricardo39985@gmail.com>
2020-03-21 22:28:34 -0400

    Changed revenue column to integer

--- db/migrate/20200315173954_create_manufacturers.rb
+++ db/migrate/20200315173954_create_manufacturers.rb
@@ -2,7 +2,7 @@ class CreateManufacturers < ActiveRecord::Migration[6.0]
   def change
     create_table :manufacturers do |t|
       t.string :ceo
-      t.string :revenue
+      t.integer :revenue
       t.string :name
 
       t.timestamps
