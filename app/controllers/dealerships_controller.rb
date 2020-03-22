9a381d9bb56dbd657b9dbf9928a0481e0f46c527
ricardo39985 <ricardo39985@gmail.com>
2020-03-21 22:58:36 -0400

    Fixed logic in controller

--- app/controllers/dealerships_controller.rb
+++ app/controllers/dealerships_controller.rb
@@ -2,40 +2,35 @@
 
 class DealershipsController < ApplicationController
   def index
-    redirect_if_try_to_spoof
     check_if_authorized
   end
 
   def new
     check_if_authorized
-    @deal = @car.dealerships.new
-    # byebug
   end
 
   def create
     check_if_authorized
     @deal = @car.dealerships.new(dealer_params)
+
     if @deal.save
       @deal.cars << @car
-      flash[:notice] = "Dealer Saved"
-       redirect_to user_car_dealerships_path(current_user, @car)
+      flash[:notice] = 'Dealer Saved'
+      redirect_to user_car_dealerships_path(current_user, @car)
     else
-      render :new      
-    end   
+      render :new
+    end
   end
-  
+
   private
+
   def dealer_params
     params.require(:dealership).permit(:name, :location)
   end
-  
+
   def check_if_authorized
     redirect_if_try_to_spoof
-    @car= current_user.cars.find_by(id: params[:car_id])
-    unless @car
-      redirect_to user_path(current_user)
-    end
-  
+    @car = current_user.cars.find_by(id: params[:car_id])
+    @car ? (@deal = @car.dealerships.new) : (redirect_to current_user)
   end
-  
 end
