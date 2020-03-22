084c36662b440cd2d5ec57cfb8d44be42671ddc1
ricardo39985 <ricardo39985@gmail.com>
2020-03-21 22:03:57 -0400

    Added Validations for model

--- app/models/dealership.rb
+++ app/models/dealership.rb
@@ -2,4 +2,10 @@ class Dealership < ApplicationRecord
     has_and_belongs_to_many :cars
     has_many :users, through: :cars
     has_many :manufacturers, through: :cars
+    validates :name, presence: true
+    validates :location, presence: true
+
+    
+    
+    
 end
