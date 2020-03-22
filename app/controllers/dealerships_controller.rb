dde3cc866d18dcc0078535721f2ed632eeda1dcc
ricardo39985 <ricardo39985@gmail.com>
2020-03-21 22:20:26 -0400

    Changed logic to fix dealer and cars not getting associated

--- app/controllers/dealerships_controller.rb
+++ app/controllers/dealerships_controller.rb
@@ -1,9 +1,41 @@
+# frozen_string_literal: true
+
 class DealershipsController < ApplicationController
   def index
-    redirect_if_doesnt_own_object
+    redirect_if_try_to_spoof
+    check_if_authorized
   end
 
-  def show
-    redirect_if_doesnt_own_object
+  def new
+    check_if_authorized
+    @deal = @car.dealerships.new
+    # byebug
   end
+
+  def create
+    check_if_authorized
+    @deal = @car.dealerships.new(dealer_params)
+    if @deal.save
+      @deal.cars << @car
+      flash[:notice] = "Dealer Saved"
+       redirect_to user_car_dealerships_path(current_user, @car)
+    else
+      render :new      
+    end   
+  end
+  
+  private
+  def dealer_params
+    params.require(:dealership).permit(:name, :location)
+  end
+  
+  def check_if_authorized
+    redirect_if_try_to_spoof
+    @car= current_user.cars.find_by(id: params[:car_id])
+    unless @car
+      redirect_to user_path(current_user)
+    end
+  
+  end
+  
 end
