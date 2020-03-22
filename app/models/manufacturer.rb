028b566fb86d005591825c6ce97e98d7f3a1c420
ricardo39985 <ricardo39985@gmail.com>
2020-03-21 22:04:29 -0400

    Added validations

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -2,4 +2,6 @@ class Manufacturer < ApplicationRecord
     has_many :cars
     has_many :users, through: :cars
     has_many :dealerships, through: :cars
+    validates :name, presence: true, uniqueness: true
+    validates :location, presence: true, uniqueness: true
 end
