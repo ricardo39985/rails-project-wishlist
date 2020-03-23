0baa099aff88418eb70e55f95f052fd78776adab
ricardo39985 <ricardo39985@gmail.com>
2020-03-22 20:57:34 -0400

    Cleaned up model

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Manufacturer < ApplicationRecord
-    has_many :cars
-    has_many :users, through: :cars
-    has_many :dealerships, through: :cars
-    validates :name, presence: true, uniqueness: true
-    validates :location, presence: true, uniqueness: true
+  has_many :cars
+  has_many :users, through: :cars
+  has_many :dealerships, through: :cars
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true, uniqueness: true
 end
