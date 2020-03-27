fd2e3dc7b93716241d54185de3596ed9056bd2f2
rick <github email address>
2020-03-26 21:01:27 -0400

    Added filter route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, skip: [:inbound_email], controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+  get '/dealership_filtered', to: 'dealerships#filter'
+  devise_for :users,
+             skip: %i[inbound_email],
+             controllers: {
+               registrations: 'registrations',
+               omniauth_callbacks: 'users/omniauth_callbacks'
+             }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: 'home#index'
   resources :users, only: %i[show index] do
     resources :cars do
       resources :dealerships, only: %i[index new create]
-      resources :manufacturers, only: [:show]
+      resources :manufacturers, only: %i[show]
     end
   end
 
